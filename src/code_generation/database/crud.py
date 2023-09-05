5fe478fe94276e54b17d907a743241249a710a05
Juan Romero <juan.romero1201@gmail.com>
2023-09-05 18:13:55 -0500

    Feat: Add UUID validator

--- src/code_generation/database/crud.py
+++ src/code_generation/database/crud.py
@@ -1,9 +1,12 @@
 from sqlalchemy.orm import Session
 from .config import engine
 from .models import PepperTest
-
+from .validators import is_valid_uuid
 
 def get_test(test_id: str):
+    if not is_valid_uuid(test_id):
+        raise Exception(f"Test id {test_id} is not a valid UUID")
+
     with Session(bind=engine) as session:
         test = session.query(PepperTest).filter(PepperTest.id == test_id).first()
         if not test:
@@ -27,6 +30,8 @@ def update_test(test: PepperTest):
 
 
 def delete_test(test_id: str):
+    if not is_valid_uuid(test_id):
+        raise Exception(f"Test id {test_id} is not a valid UUID")
     with Session(bind=engine) as session:
         test = session.query(PepperTest).filter(PepperTest.id == test_id).first()
         if not test:
