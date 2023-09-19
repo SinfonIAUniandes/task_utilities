a680a6a7299744798c70ac3dc933d600bd29aa1f
luccasrojas <l.rojasb@uniandes.edu.co>
2023-09-18 20:33:22 -0500

    class

--- src/code_generation/main.py
+++ src/code_generation/main.py
@@ -2,11 +2,6 @@ from database.config import init_db
 from dotenv import load_dotenv
 from generate_utils import load_code_gen_config
 import test_module
-
-load_dotenv()
-init_db()
-load_code_gen_config()
-
 import time
 import json
 from traceback import format_exception
@@ -15,36 +10,45 @@ from generate import generate_code as gen
 from database.models import Model, PromptingType, ExecutionResults
 from database.crud import get_non_executed_tests, update_test
 
-def evaluate_automated_tests(num_tests:int=10, prompting_type = PromptingType.CHAINING, model=Model.GPT35):
-    tasks = get_non_executed_tests(num_tests)
-    for task in tasks:
-        description = task.task
-        model_response = None
-        t1 = time.time()
-        if prompting_type == PromptingType.CHAINING:
-            steps, code = chain_gen(description, model)
-            model_response = json.dumps({"steps": steps, "code": code})
-        else:
-            code = gen(description, model)
-            model_response = json.dumps({"code": code})
-        t2 = time.time()
+class CodeGeneration:
 
-        generation_time = t2-t1
-        exception_message = None
-        execution = ExecutionResults.NOT_EXECUTED
-        try:
-            tm = test_module.Task_module(percception = True, speech = True, manipulation = True, navigation = True)
-            exec(code)
-            execution = ExecutionResults.EXECUTED
-        except Exception as e:
-            execution = ExecutionResults.EXECUTED_BUT_FAILED
-            exception_message = "".join(format_exception(type(e), e, e.__traceback__))
+    def __init__(self):
+        load_dotenv()
+        init_db()
+        load_code_gen_config()
+        self.tm = test_module.Task_module(percception = True, speech = True, manipulation = True, navigation = True)
 
-        task.model_response = model_response
-        task.task_execution_result = execution
-        task.raised_error = exception_message
-        task.generation_time_ms = generation_time
-        update_test(task)
+    def evaluate_automated_tests(self,num_tests:int=10, prompting_type = PromptingType.CHAINING, model=Model.GPT35):
+        tasks = get_non_executed_tests(num_tests)
+        for task in tasks:
+            description = task.task
+            model_response = None
+            t1 = time.time()
+            if prompting_type == PromptingType.CHAINING:
+                steps, code = chain_gen(description, model)
+                model_response = json.dumps({"steps": steps, "code": code})
+            else:
+                code = gen(description, model)
+                model_response = json.dumps({"code": code})
+            t2 = time.time()
+
+            generation_time = t2-t1
+            exception_message = None
+            execution = ExecutionResults.NOT_EXECUTED
+            try:
+                
+                exec(code)
+                execution = ExecutionResults.EXECUTED
+            except Exception as e:
+                execution = ExecutionResults.EXECUTED_BUT_FAILED
+                exception_message = "".join(format_exception(type(e), e, e.__traceback__))
+
+            task.model_response = model_response
+            task.task_execution_result = execution
+            task.raised_error = exception_message
+            task.generation_time_ms = generation_time
+            update_test(task)
 
 if __name__ == "__main__":
-    evaluate_automated_tests()
+    cg = CodeGeneration()
+    cg.evaluate_automated_tests()
