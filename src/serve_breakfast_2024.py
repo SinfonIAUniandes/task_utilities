e24c226a5046de3939e7cdc4ef395aa2151e5d1e
SaroM0 <s.rodriguezm2@uniandes.edu.co>
2024-04-16 18:17:16 -0500

    serve_breakfast_2024 fixes

--- src/serve_breakfast_2024.py
+++ src/serve_breakfast_2024.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 import os
+import time
 import rospy
 import threading
-import numpy as np
 import ConsoleFormatter
 
 from transitions import Machine
@@ -11,14 +11,14 @@ from task_module import Task_module as tm
 class SERVE_BREAKFAST(object):
     def __init__(self) -> None:
         self.consoleFormatter = ConsoleFormatter.ConsoleFormatter()
-        self.tm = tm(navigation=True, manipulation=True, speech=True, perception = True, pytoolkit=True)
+        self.tm = tm(navigation=True, manipulation=True, speech=True, perception = False, pytoolkit=True)
         self.tm.initialize_node('SERVE_BREAKFAST')
-
-        self.STATES = ['INIT', 'GO_2_CUPBOARD', 'LOOK_FOR_OBJECT' ,'GRAB_OBJECT', 'GO_DROP_PLACE', 'DROP_OBJECT', 'MAKE_BREAKFAST', 'END']
-        self.TRANSITIONS = [{'trigger': 'init', 'source': 'SERVE_BREAKFAST', 'dest': 'INIT'},
+ 
+        self.STATES = ['INIT', 'GO_2_CUPBOARD', 'LOOK_FOR_OBJECTS' ,'GRAB_OBJECT', 'GO_DROP_PLACE', 'DROP_OBJECT', 'MAKE_BREAKFAST', 'END']
+        self.TRANSITIONS = [{'trigger': 'zero', 'source': 'SERVE_BREAKFAST', 'dest': 'INIT'},
                             {'trigger': 'start', 'source': 'INIT', 'dest': 'GO_2_CUPBOARD'},
-                            {'trigger': 'look_for_object', 'source': 'GO_2_CUPBOARD', 'dest': 'LOOK_FOR_OBJECT'},
-                            {'trigger': 'grab_ingredient', 'source': 'LOOK_FOR_OBJECT', 'dest': 'GRAB_OBJECT'},
+                            {'trigger': 'look_for_object', 'source': 'GO_2_CUPBOARD', 'dest': 'LOOK_FOR_OBJECTS'},
+                            {'trigger': 'grab_ingredient', 'source': 'LOOK_FOR_OBJECTS', 'dest': 'GRAB_OBJECT'},
                             {'trigger': 'go_drop_place', 'source': 'GRAB_OBJECT', 'dest': 'GO_DROP_PLACE'},
                             {'trigger': 'drop_object', 'source': 'GO_DROP_PLACE', 'dest': 'DROP_OBJECT'},
                             {'trigger': 'again', 'source': 'DROP_OBJECT', 'dest': 'GO_GRAB_PLACE'},
@@ -32,26 +32,29 @@ class SERVE_BREAKFAST(object):
 
 
     def on_enter_INIT(self):
+        self.tm.go_to_pose("standard")
+        self.tm.go_to_pose("default_head")
         self.tm.talk("I will serve breakfast")
+        time.sleep(5)
         self.start()
 
-    def on_enter_GO_GRAB_PLACE(self):
+    def on_enter_GO_2_CUPBOARD(self):
         #TODO Poner el lugar de destino para recoger las cosas cuando este listo el mapa
         self.tm.talk("I am going to go pick up the stuff for breakfast")
         self.tm.go_to_place("")
         self.look_for_object()
 
     def on_enter_LOOK_FOR_OBJECTS(self):
-        self.tm.go_to_state("down_head")
+        self.tm.go_to_pose("down_head")
         ingredients_prompt = f"Just give me in a python list the objects that are considered realted with food
         or ingredients that you see below, from left to right. Do not answer aboslutely anything else. For example the answer
         will be [milk, cereal, spon, bowl]"
         self.ingredients = list(self.tm.img_description(ingredients_prompt)["message"])
-        self.tm.talk("I can see ", "English", wait=False)
+        self.tm.talk("I can see ")
         for i in self.ingredients:
             self.tm.talk( f"a {str(i)}", "English", wait=False)
         self.j = 0
-        self.grab_object()
+        self.grab_ingredient()
 
     def on_enter_GRAB_OBJECT(self):
         object_prompt = f"Just tell me the number of what I am going to ask you, don't answer anything else.
@@ -61,18 +64,25 @@ class SERVE_BREAKFAST(object):
         your answer will be 1 or 0 or -1, etc"
         self.approach = self.tm.img_description(object_prompt)["message"]
         self.tm.go_to_relative_point(0,self.approach,0)
-        if self.ingredients[self.j] == "milk":
-            # TODO crear y añadir la pose correspondiente
-            self.tm.play_action("")
-        elif self.ingredients[self.j] == "bowl":
-            # TODO crear y añadir la pose correspondiente
-            self.tm.play_action("")
-        elif self.ingredients[self.j] == "cereal":
-            # TODO crear y añadir la pose correspondiente
-            self.tm.play_action("")
-        elif self.ingredients[self.j] == "spon":
-            # TODO crear y añadir la pose correspondiente
-            self.tm.play_action("")
+        # if self.ingredients[self.j] == "milk":
+        #     # TODO crear y añadir la pose correspondiente
+        #     self.tm.play_action("")
+        # elif self.ingredients[self.j] == "bowl":
+        #     # TODO crear y añadir la pose correspondiente
+        #     self.tm.play_action("")
+        # elif self.ingredients[self.j] == "cereal":
+        #     # TODO crear y añadir la pose correspondiente
+            self.tm.go_to_pose("both_arms")
+            time.sleep(10)
+            self.tm.go_to_pose("close_both_arms")
+            time.sleep(5)
+            self.tm.go_to_pose("raise_closed_arms")
+            time.sleep(10)
+            self.tm.go_to_pose("default_head")
+        #     self.tm.play_action("")
+        # elif self.ingredients[self.j] == "spon":
+        #     # TODO crear y añadir la pose correspondiente
+        #     self.tm.play_action("")
         self.go_drop_place()
 
     def on_enter_GO_DROP_PLACE(self):
@@ -83,7 +93,9 @@ class SERVE_BREAKFAST(object):
 
     def on_enter_DROP_OBJECT(self):
         #TODO Crear y añadir pose para soltar objeto
-        self.tm.play_action("")
+        self.tm.go_to_pose("both_arms")
+        time.sleep(2)
+        self.tm.go_to_pose("standard")
         if self.ingredients[self.j] == self.ingredients[-1]:
             self.make_breakfast()
         else:
