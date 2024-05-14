ff8bb078f706d4fd2e34c0a66cc86e19adcd75e5
Dakudemon23 <jdmontenegroc32@gmail.com>
2024-05-14 17:26:38 -0500

    declaracion de parametros

--- src/clean_the_table_2024.py
+++ src/clean_the_table_2024.py
@@ -16,10 +16,16 @@ class CLEAN_THE_TABLE(object):
         self.tm.initialize_node('CLEAN_THE_TABLE')
  
         #TODO Definir estados
-        self.STATES = ['INIT', 'END'] 
+        self.STATES = ['INIT', 'GO_TO_TABLE', 'GRAB_OBJECT', 'GO_DISHWASHER', 'DROP_OBJECT', 'CLEAN', 'END'] 
         #TODO Definir transiciones entre los estados
-        self.TRANSITIONS = [{'trigger': 'zero', 'source': 'CLEAN_THE_TABLE', 'dest': 'STATE_0'},
-                            {'trigger': 'finish', 'source': 'STATE_0', 'dest': 'END'}]
+        self.TRANSITIONS = [{'trigger': 'zero', 'source': 'CLEAN_THE_TABLE', 'dest': 'INIT'},
+                            {'trigger': 'start', 'source': 'INIT', 'dest': 'GO_TO_TABLE'},
+                            {'trigger': 'grab_ingredient', 'source': 'GO_TO_TABLE', 'dest': 'GRAB_OBJECT'},
+                            {'trigger': 'go_drop_place', 'source': 'GRAB_OBJECT', 'dest': 'GO_DISHWASHER'},
+                            {'trigger': 'drop_object', 'source': 'GO_DISHWASHER', 'dest': 'DROP_OBJECT'},
+                            {'trigger': 'again', 'source': 'DROP_OBJECT', 'dest': 'GO_TO_TABLE'},
+                            {'trigger': 'clean_table', 'source': 'DROP_OBJECT', 'dest': 'CLEAN'},
+                            {'trigger': 'end', 'source': 'CLEAN', 'dest': 'END'}]
         
         self.machine = Machine(model=self, states=self.STATES, transitions=self.TRANSITIONS, initial='CLEAN_THE_TABLE')
         
@@ -31,6 +37,14 @@ class CLEAN_THE_TABLE(object):
         # -------------------------------------------------------------------------------------------------------------------------------------------
         
         #TODO Parametrizar variables para que sea mas facil editar
+        # Movements Speed
+        self.fast_movement = 0.15
+        self.normal_movement = 0.1
+        self.slow_movement = 0.05
+
+        self.item = 0 # Contador para saber que ingrediente se esta manipulando
+        self.items = ["cup", "bowl", "spoon", "fork", "dish"] # Orden de los ingredientes (izquierda a derecha)
+        self.distance_between_items = 0.2 # Distancia promedio entre los objetos en la cupboard
 
         
         # -------------------------------------------------------------------------------------------------------------------------------------------
@@ -39,12 +53,20 @@ class CLEAN_THE_TABLE(object):
 
     #TODO Crear logica y estados para la task
     def on_enter_INIT(self): 
+        self.tm.set_current_place("init")
+        self.tm.go_to_pose("standard", self.fast_movement)
+        self.tm.go_to_pose("default_head", self.fast_movement)
+        self.tm.set_security_distance(False)
+        self.tm.initialize_pepper()
+        self.tm.go_to_pose("up_head", 0.1)
+        self.tm.talk("Have you finished eating? Let me clean the table.", "English", wait=True)
         self.start()
         
     def on_enter_STATE_0(self): 
         self.finish()
 
     def on_enter_END(self):
+        self.tm.talk("I finished cleaning the table")
         return
 
     def check_rospy(self):
