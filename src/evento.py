82f5bd0fc88d0dcd683b4129fcfd9a40171d1aad
SinfonIAPepperTeam <sinfonia@uniandes.edu.co>
2024-08-09 11:18:12 -0500

    add move hand to evento made pose maker task

---
+++ src/evento.py
@@ -0,0 +1,355 @@
+#!/usr/bin/env python3
+from transitions import Machine
+from task_module import Task_module as tm
+from datetime import datetime
+import ConsoleFormatter
+import threading
+import pandas as pd
+import rospy
+import random
+import os
+import csv
+
+from std_srvs.srv import SetBool
+from robot_toolkit_msgs.msg import speech_recognition_status_msg, animation_msg, motion_tools_msg, leds_parameters_msg, touch_msg
+from robot_toolkit_msgs.srv import tablet_service_srv,  set_open_close_hand_srv, set_open_close_hand_srvRequest, motion_tools_srv, battery_service_srv, set_output_volume_srv, tablet_service_srvRequest, set_stiffnesses_srv, set_stiffnesses_srvRequest
+
+class Evento(object):
+    def __init__(self):
+        # TODO
+        """
+        - Revisar si se meten los servicios de interface o del pytoolkit directamente (ojala nodo de interface)
+        - Falta meter todos los servicios del pytoolkit al modulo para que se puedan llamar facilmente desde la maquina de estados.
+        - Falta crear behaviours como el spin_until_object que es usado varias veces en varios tasks.
+        - Falta revisar todos los angulos de navegacion al hacer look_4_something y la velocidad de giro 
+        - Poner una redundancia para cuando el robot asigna un nuevo a id a una misma persona para que no la presente (lista con los nombres de los que ya presento, incluyendo el actual)
+        """
+
+        self.consoleFormatter=ConsoleFormatter.ConsoleFormatter()
+        # Definir los estados posibles del semáforo
+        self.task_name = "Event"
+        self.hearing = True
+        self.is_done = False
+        self.hey_pepper=False
+        self.already_asereje = False 
+        self.already_dance = False 
+        self.haciendo_animacion = False
+        states = ['INIT', 'WAIT4GUEST', 'TALK']
+        self.tm = tm(perception = False,speech=True, pytoolkit=True)
+        self.tm.initialize_node(self.task_name)
+        # Definir las transiciones permitidas entre los estados
+        transitions = [
+            {'trigger': 'start', 'source': 'TALK', 'dest': 'INIT'},
+            {'trigger': 'beggining', 'source': 'INIT', 'dest': 'WAIT4GUEST'},
+            {'trigger': 'person_arrived', 'source': 'WAIT4GUEST', 'dest': 'TALK'},
+            {'trigger': 'TALK_done', 'source': 'TALK', 'dest': 'WAIT4GUEST'}
+        ]
+        
+        # Crear la máquina de estados
+        self.machine = Machine(model=self, states=states, transitions=transitions, initial='TALK')
+        rospy_check = threading.Thread(target=self.check_rospy)
+        rospy_check.start()
+        ############################# GLOBAL VARIABLES #############################
+
+    def on_enter_INIT(self):
+        self.tm.talk("Iniciando modo de Demostración","Spanish")
+        print(self.consoleFormatter.format("Inicializacion del task: "+self.task_name, "HEADER"))
+        self.tm.initialize_pepper()
+        subscriber = rospy.Subscriber("/pytoolkit/ALSpeechRecognition/status",speech_recognition_status_msg,self.callback_hot_word)
+        self.motion_tools_service()
+        self.enable_breathing_service()
+        self.enable_hot_word_service()
+        print(self.consoleFormatter.format("Waiting for pytoolkit/ALTabletService/show_picture_srv...", "WARNING"))
+        rospy.wait_for_service('pytoolkit/ALTabletService/show_picture_srv')
+        print(self.consoleFormatter.format("Waiting for /pytoolkit/ALTabletService/show_image_srv...", "WARNING"))
+        rospy.wait_for_service("/pytoolkit/ALTabletService/show_image_srv")
+        self.show_image_srv = rospy.ServiceProxy("/pytoolkit/ALTabletService/show_image_srv",tablet_service_srv)
+        self.show_picture_proxy = rospy.ServiceProxy('pytoolkit/ALTabletService/show_picture_srv', battery_service_srv)
+        self.play_dance_srv = rospy.ServiceProxy("/pytoolkit/ALMotion/play_dance_srv", set_output_volume_srv)
+        self.animationPublisher = rospy.Publisher('/animations', animation_msg, queue_size=10)
+        self.motion_set_stiffnesses_client = rospy.ServiceProxy("pytoolkit/ALMotion/set_stiffnesses_srv", set_stiffnesses_srv)
+        req_stiffnesses = set_stiffnesses_srvRequest()
+        req_stiffnesses.names = "Body"
+        req_stiffnesses.stiffnesses = 1
+        self.motion_set_stiffnesses_client(req_stiffnesses)
+        self.set_arms_security = rospy.ServiceProxy("pytoolkit/ALMotion/set_arms_security_srv", SetBool)
+        self.set_arms_security(False)
+        self.headSensorSubscriber = rospy.Subscriber("/touch", touch_msg, self.callback_head_sensor_subscriber)
+        rospy.wait_for_service("/pytoolkit/ALTabletService/show_image_srv")
+        self.beggining()
+
+    def on_enter_TALK(self):
+        print(self.consoleFormatter.format("TALK", "HEADER"))
+        anim_msg = self.gen_anim_msg("Gestures/BowShort_3")
+        self.animationPublisher.publish(anim_msg)
+        self.hearing = False
+        self.tm.talk("Bienvenido, soy Nova, es un gusto conocerte","Spanish",animated=False)
+        self.tm.talk("Di Hey Nova cuando quieras decirme algo, y chao cuando no quieras seguir hablando","Spanish",animated=True)
+        rospy.sleep(0.9)
+        self.tm.start_tracker_proxy()
+        self.hearing = True
+        while not self.is_done:
+            if self.hey_pepper:
+                self.tm.show_words_proxy()
+                self.hey_pepper_function()
+                self.hey_pepper=False
+            rospy.sleep(0.1)
+        self.tm.talk("Adios","Spanish",animated=True)
+        self.TALK_done()
+
+    def on_enter_WAIT4GUEST(self):
+        self.tm.show_words_proxy()
+        self.is_done=False
+        print(self.consoleFormatter.format("WAIT4GUEST", "HEADER"))
+        self.tm.setRPosture_srv("stand")
+        self.tm.setMoveHead_srv.call("up")
+        self.tm.stop_tracker_proxy()
+        self.person_arrived()
+
+    def check_rospy(self):
+        #Termina todos los procesos al cerrar el nodo
+        while not rospy.is_shutdown():
+            rospy.sleep(0.1)
+        print(self.consoleFormatter.format("Shutting down", "FAIL"))
+        os._exit(os.EX_OK)
+
+    def run(self):
+        while not rospy.is_shutdown():
+            self.start()
+    
+    def enable_hot_word_service(self):
+        """
+        Enables hotwords detection from the toolkit of the robot.
+        """
+        print("Waiting for hot word service")
+        rospy.wait_for_service('/pytoolkit/ALSpeechRecognition/set_hot_word_language_srv')
+        try:
+            hot_word_language_srv = rospy.ServiceProxy("/pytoolkit/ALSpeechRecognition/set_hot_word_language_srv", tablet_service_srv)
+            hot_word_language_srv("Spanish")
+            self.set_hot_words()
+            print("Hot word service connected!")
+        except rospy.ServiceException as e:
+            print("Service call failed")
+    
+    def enable_breathing_service(self):
+        """
+        Enables the breathing animations of the robot.
+        """
+        request = set_open_close_hand_srvRequest()
+        request.hand = "All"
+        request.state = "True"
+        print("Waiting for breathing service")
+        rospy.wait_for_service("/pytoolkit/ALMotion/toggle_breathing_srv")
+        try:
+            toggle_breathing_proxy = rospy.ServiceProxy("/pytoolkit/ALMotion/toggle_breathing_srv", set_open_close_hand_srv)
+            toggle_breathing_proxy(request)
+            print("Breathing service connected!")
+        except rospy.ServiceException as e:
+            print("Service call failed")
+        request = set_open_close_hand_srvRequest()
+        request.hand = "Head"
+        request.state = "False"
+        print("Waiting for breathing service")
+        rospy.wait_for_service("/pytoolkit/ALMotion/toggle_breathing_srv")
+        try:
+            toggle_breathing_proxy = rospy.ServiceProxy("/pytoolkit/ALMotion/toggle_breathing_srv", set_open_close_hand_srv)
+            toggle_breathing_proxy(request)
+            print("Breathing service connected!")
+        except rospy.ServiceException as e:
+            print("Service call failed")
+    
+    def motion_tools_service(self):
+        """
+        Enables the motion Tools service from the toolkit of the robot.
+        """
+        request = motion_tools_msg()
+        request.command = "enable_all"
+        print("Waiting for motion tools service")
+        rospy.wait_for_service('/robot_toolkit/motion_tools_srv')
+        try:
+            motion = rospy.ServiceProxy('/robot_toolkit/motion_tools_srv', motion_tools_srv)
+            motion(request)
+            print("Motion tools service connected!")
+        except rospy.ServiceException as e:
+            print("Service call failed")
+            
+    
+    def hey_pepper_function(self):
+        self.tm.hot_word([])
+        self.tm.talk("Dímelo manzana","Spanish",animated=True,wait=False)
+        rospy.sleep(1)
+        text = self.tm.speech2text_srv(seconds=0,lang="esp")
+        anim_msg = self.gen_anim_msg("Waiting/Think_3")
+        self.animationPublisher.publish(anim_msg)
+        if not ("None" in text):
+            request = f"""La persona dijo: {text}. Si hay palabras en otro idioma en tu respuesta escribelas como se pronunicarian en español porque en este momento solo puedes hablar español y ningun otro idioma, por ejemplo si en tu respuesta esta Python, responde Paiton. No añadas contenido complejo a tu respuesta como codigo, solo explica lo que sea necesario."""
+            if "hoy" in text or "día" in text:
+                fecha_actual = datetime.now()
+                print(datetime.now())
+                dia = fecha_actual.day
+                mes = fecha_actual.month
+                m = ['nan','enero','febrero','marzo','abril','mayo','junio','julio','agosto','septiembre','octubre','noviembre','diciembre']
+                answer="Hoy es: {} de {}".format(dia, m[mes])
+            elif "hora" in text or "horas" in text:
+                fecha_actual = datetime.now()
+                hora = fecha_actual.hour
+                minuto = fecha_actual.minute
+                answer="Son las: {} y {} minutos".format(hora, minuto)
+            elif "baila" in text.lower() or "baile" in text.lower():
+                answer = "Me encanta bailar!"
+                word_msg = speech_recognition_status_msg()
+                options = ["baile", "asereje"]
+                word_msg.status = random.choice(options)
+                self.callback_hot_word(word_msg)
+            elif "darme la mano" in text.lower() or "dame la mano" in text.lower() or "da la mano" in text.lower():
+                answer = "Claro que si, toma cualquier mano y aprieta suavemente la parte de afuera!"
+            else:
+                answer=self.tm.answer_question(request, save_conversation=True) 
+            self.tm.talk(answer,"Spanish",animated=True, wait=True)
+            if "beso" in text.lower():
+                word_msg = speech_recognition_status_msg()
+                word_msg.status = "beso"
+                self.callback_hot_word(word_msg)
+            elif "gracias" in text.lower():
+                word_msg = speech_recognition_status_msg()
+                word_msg.status = "gracias"
+                self.callback_hot_word(word_msg)
+            elif "musculo" in text.lower():
+                word_msg = speech_recognition_status_msg()
+                word_msg.status = "musculos"
+                self.callback_hot_word(word_msg)
+            elif "zombi" in text.lower():
+                word_msg = speech_recognition_status_msg()
+                word_msg.status = "zombi"
+                self.callback_hot_word(word_msg)
+            elif "guitarra" in text.lower():
+                word_msg = speech_recognition_status_msg()
+                word_msg.status = "guitarra"
+                self.callback_hot_word(word_msg)
+        else:
+            self.tm.talk("Disculpa, no te entendi, puedes hablar cuando mis ojos esten azules. Por favor habla mas lento","Spanish",animated=True)
+        self.set_hot_words()
+
+    def set_hot_words(self):
+        if self.hearing:
+            #self.tm.hot_word(["chao","detente","hey nova","baile","asereje","pose","musculos" ,"besos","foto","guitarra","cumpleaños","corazon","llama","helicoptero","zombi","carro","gracias"],thresholds=[0.45, 0.4, 0.398, 0.43, 0.43, 0.39, 0.4, 0.4, 0.5, 0.4, 0.4, 0.4, 0.41, 0.4, 0.4, 0.43, 0.4])
+            self.tm.hot_word(["hey nova", "chao"],thresholds=[0.375, 0.47])
+            
+
+    def callback_head_sensor_subscriber(self, msg: touch_msg):
+        self.tm.hot_word([])
+        req_stiffnesses = set_stiffnesses_srvRequest()
+        if "hand_left_back" in msg.name:
+            req_stiffnesses.names = "LArm"
+        elif "hand_right_back" in msg.name:  
+            req_stiffnesses.names = "RArm"
+        req_stiffnesses.stiffnesses = 0 if msg.state else 1
+        self.motion_set_stiffnesses_client(req_stiffnesses)
+        if msg.state:
+            self.tm.talk("Sosten firmemente ahi para mover mi brazo", language="Spanish", wait=True)
+        else:
+            self.tm.talk("Soltaste mi mano", language="Spanish", wait=True)
+        self.tm.hot_word(["hey nova", "chao"],thresholds=[0.375, 0.47])
+
+    def gen_anim_msg(self, animation):
+        anim_msg = animation_msg()
+        anim_msg.family = "animations"
+        anim_msg.animation_name = animation
+        return anim_msg
+    
+    def dance_threadf(self, number):
+        #Se ponen los bailes en un thread porque estancan la ejecucion y no permiten checkear otras hotwords, como detente
+        self.haciendo_animacion = True
+        self.play_dance_srv(number)
+        self.haciendo_animacion = False
+    
+    def setLedsColor(self, r,g,b):
+        """
+        Function for setting the colors of the eyes of the robot.
+        Args:
+        r,g,b numbers
+            r for red
+            g for green
+            b for blue
+        """
+        ledsPublisher = rospy.Publisher('/leds', leds_parameters_msg, queue_size=10)
+        ledsMessage = leds_parameters_msg()
+        ledsMessage.name = "FaceLeds"
+        ledsMessage.red = r
+        ledsMessage.green = g
+        ledsMessage.blue = b
+        ledsMessage.time = 0
+        ledsPublisher.publish(ledsMessage)  #Inicio(aguamarina), Pepper esta ALSpeechRecognitionStatusPublisherlista para escuchar
+    
+    def callback_hot_word(self,data):
+        word = data.status
+        print(word, "listened")
+        if word=="detente":
+            self.tm.setRPosture_srv("stand")
+            self.haciendo_animacion = False
+            
+        if not self.haciendo_animacion:
+            self.haciendo_animacion = True
+            if word == "chao":
+                self.is_done = True
+                self.already_dance = False
+                self.already_asereje = False
+                self.tm.answer_question("", save_conversation=False) 
+            elif word == "hey nova":
+                self.hey_pepper = True
+            elif word == "guitarra":
+                anim_msg = self.gen_anim_msg("Waiting/AirGuitar_1")
+                self.animationPublisher.publish(anim_msg)
+            elif word == "besos":
+                anim_msg = self.gen_anim_msg("Gestures/Kisses_1")
+                self.animationPublisher.publish(anim_msg)
+                self.tm.talk("Muah!","Spanish")
+            elif word == "baile":
+                if not self.already_dance:
+                    dance_thread = threading.Thread(target=self.dance_threadf,args=[1])
+                    dance_thread.start()
+                    self.already_dance = False
+            elif word == "asereje":
+                if not self.already_asereje:
+                    dance_thread = threading.Thread(target=self.dance_threadf,args=[3])
+                    dance_thread.start()
+                    self.already_asereje = True
+            elif word == "pose":
+                anim_msg = self.gen_anim_msg("Gestures/ShowSky_8")
+                self.animationPublisher.publish(anim_msg)
+            elif word == "foto":
+                anim_msg = self.gen_anim_msg("Waiting/TakePicture_1")
+                self.animationPublisher.publish(anim_msg)
+                rospy.sleep(2)
+                self.show_picture_proxy()
+            elif word == "cumpleaños":
+                anim_msg = self.gen_anim_msg("Waiting/HappyBirthday_1")
+                self.animationPublisher.publish(anim_msg)
+            elif word == "corazon":
+                anim_msg = self.gen_anim_msg("Waiting/LoveYou_1")
+                self.animationPublisher.publish(anim_msg)
+            elif word == "llama":
+                anim_msg = self.gen_anim_msg("Waiting/CallSomeone_1")
+                self.animationPublisher.publish(anim_msg)
+            elif word == "helicoptero":
+                anim_msg = self.gen_anim_msg("Waiting/Helicopter_1")
+                self.animationPublisher.publish(anim_msg)
+            elif word == "zombi":
+                anim_msg = self.gen_anim_msg("Waiting/Zombie_1")
+                self.animationPublisher.publish(anim_msg)
+            elif word == "carro":
+                anim_msg = self.gen_anim_msg("Waiting/DriveCar_1")
+                self.animationPublisher.publish(anim_msg)
+            elif word == "musculos":
+                anim_msg = self.gen_anim_msg("Waiting/ShowMuscles_3")
+                self.animationPublisher.publish(anim_msg)
+            elif word == "gracias":
+                anim_msg = self.gen_anim_msg("Gestures/BowShort_3")
+                self.animationPublisher.publish(anim_msg)
+            self.haciendo_animacion = False
+    
+# Crear una instancia de la maquina de estados
+if __name__ == "__main__":
+    sm = Evento()
+    sm.run()
+    rospy.spin()
---
+++ src/pose_maker.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python3
+
+# --------------- GENERAL LIBRARIES IMPORTS ---------------
+import os
+import time
+import rospy
+import threading
+import ConsoleFormatter
+from transitions import Machine
+from task_module import Task_module as tm
+
+from std_srvs.srv import SetBool
+from robot_toolkit_msgs.srv import set_stiffnesses_srv, set_stiffnesses_srvRequest
+from robot_toolkit_msgs.msg import leds_parameters_msg, touch_msg
+
+# --------------- MESSAGES AND SERVICES IMPORTS ---------------
+from std_msgs.msg import String
+
+# --------------- CARRY MY LUGGAGE TASK CLASS DEFINITION ---------------
+class CARRY_MY_LUGGAGE(object):
+    
+    # --------------- Constructor ---------------    
+    def __init__(self) -> None:
+        self.consoleFormatter = ConsoleFormatter.ConsoleFormatter()
+        self.tm = tm(navigation=False, manipulation=False, perception=False, speech=True, pytoolkit=True)
+        self.tm.initialize_node("pose_generator")
+
+        # --------------- Machine States ---------------
+
+        self.STATES = [
+            "INIT",
+            "LOOK_FOR_BAG",
+            "GRAB_BAG",
+            "FOLLOW_YOU",
+            "GO_BACK",
+            "END",
+        ]
+        
+        # --------------- Machine Transitions ---------------
+        
+        self.TRANSITIONS = [
+            {"trigger": "zero", "source": "CARRY_MY_LUGGAGE", "dest": "INIT"},
+            {"trigger": "start", "source": "INIT", "dest": "LOOK_FOR_BAG"},
+            {"trigger": "grab_bag", "source": "LOOK_FOR_BAG", "dest": "GRAB_BAG"},
+            {"trigger": "grab_bag_again", "source": "GRAB_BAG", "dest": "GRAB_BAG"},
+            {"trigger": "follow_you", "source": "GRAB_BAG", "dest": "FOLLOW_YOU"},
+            {"trigger": "go_back", "source": "FOLLOW_YOU", "dest": "GO_BACK"},
+            {"trigger": "end", "source": "GO_BACK", "dest": "END"},
+        ]
+        
+        # --------------- Machine Declaration ---------------
+
+        self.machine = Machine(
+            model=self,
+            states=self.STATES,
+            transitions=self.TRANSITIONS,
+            initial="CARRY_MY_LUGGAGE",
+        )
+        
+        # --------------- ROSPY Check ---------------
+        
+        self.can_move_head = False
+        rospy_check = threading.Thread(target=self.check_rospy)
+        rospy_check.start()
+
+
+    # --------------------------------------------- STATE FUNCTIONS DEFINITION ---------------------------------------------
+    
+        
+    # === Parameters whisper ===
+    def setLedsColor(self,r,g,b):
+        """
+        Function for setting the colors of the eyes of the robot.
+        Args:
+        r,g,b numbers
+            r for red
+            g for green
+            b for blue
+        """
+        ledsPublisher = rospy.Publisher('/leds', leds_parameters_msg, queue_size=10)
+        ledsMessage = leds_parameters_msg()
+        ledsMessage.name = "FaceLeds"
+        ledsMessage.red = r
+        ledsMessage.green = g
+        ledsMessage.blue = b
+        ledsMessage.time = 0
+        ledsPublisher.publish(ledsMessage)
+        rospy.sleep(0.2)
+     
+    # --------------- FIRST STATE: INIT ---------------
+    
+    def on_enter_INIT(self):
+        
+        
+        # --------------- INIT STATE PROCCEDURE ---------------
+        self.tm.show_words_proxy()
+        rospy.sleep(3)
+        print(self.consoleFormatter.format("INIT", "HEADER"))
+        self.motion_set_stiffnesses_client = rospy.ServiceProxy("pytoolkit/ALMotion/set_stiffnesses_srv", set_stiffnesses_srv)
+        req_stiffnesses = set_stiffnesses_srvRequest()
+        req_stiffnesses.names = "Body"
+        req_stiffnesses.stiffnesses = 1
+        self.motion_set_stiffnesses_client(req_stiffnesses)
+        self.set_arms_security = rospy.ServiceProxy("pytoolkit/ALMotion/set_arms_security_srv", SetBool)
+        self.set_arms_security(False)
+        self.headSensorSubscriber = rospy.Subscriber("/touch", touch_msg, self.callback_head_sensor_subscriber)
+        self.setLedsColor(165,165,0)
+        self.setLedsColor(165,165,0)
+        self.setLedsColor(165,165,0)
+        self.tm.talk("Generador de poses activado. Cuando mis ojos esten naranjas mis extremidades estan fijas, cuando estan verdes mis extremidades se pueden mover, para empezar a mover mi cabeza toca el sensor en la mitad de mi cabeza y cuando acabes vuelve a tocarlo.", language="Spanish", wait=True)
+        self.tm.talk("Para mover uno de mis brazos toca el sensor exterior y mueve el brazo con cuidado, una vez sueltes el sensor mi brazo se quedara fijo", language="Spanish", wait=True)
+        
+        
+            
+            
+        while True:
+            rospy.sleep(1)
+        
+        # Moving to LOOK_FOR_BAG state
+        self.start()
+
+
+    def callback_head_sensor_subscriber(self, msg: touch_msg):
+        if "head_middle" in msg.name:
+            if msg.state:
+                #Apagar que se pueda mover la cabeza
+                if self.can_move_head:
+                    self.tm.talk("Cabeza fija", language="Spanish", wait=False)
+                    self.can_move_head = False
+                    self.setLedsColor(165,165,0)
+                    req_stiffnesses = set_stiffnesses_srvRequest()
+                    req_stiffnesses.names = "Head"
+                    req_stiffnesses.stiffnesses = 1
+                    self.motion_set_stiffnesses_client(req_stiffnesses)
+                #Encender que se pueda mover la cabeza
+                else:
+                    self.tm.talk("Cabeza suelta", language="Spanish", wait=False)
+                    self.can_move_head = True
+                    self.setLedsColor(0,255,0)
+                    req_stiffnesses = set_stiffnesses_srvRequest()
+                    req_stiffnesses.names = "Head"
+                    req_stiffnesses.stiffnesses = 0
+                    self.motion_set_stiffnesses_client(req_stiffnesses)
+        elif "hand_left_back" in msg.name:
+            if msg.state:
+                self.tm.talk("Brazo derecho suelto", language="Spanish", wait=False)
+                self.setLedsColor(0,255,0)
+            else:
+                self.tm.talk("Brazo derecho fijo", language="Spanish", wait=False)
+                self.setLedsColor(165,165,0)
+            req_stiffnesses = set_stiffnesses_srvRequest()
+            req_stiffnesses.names = "LArm"
+            req_stiffnesses.stiffnesses = 0 if msg.state else 1
+            self.motion_set_stiffnesses_client(req_stiffnesses)
+        elif "hand_right_back" in msg.name:  
+            if msg.state:
+                self.tm.talk("Brazo izquierdo suelto", language="Spanish", wait=False)
+                self.setLedsColor(0,255,0)
+            else:
+                self.tm.talk("Brazo izquierdo fijo", language="Spanish", wait=False)
+                self.setLedsColor(165,165,0)
+            req_stiffnesses = set_stiffnesses_srvRequest()
+            req_stiffnesses.names = "RArm"
+            req_stiffnesses.stiffnesses = 0 if msg.state else 1
+            self.motion_set_stiffnesses_client(req_stiffnesses)
+
+  
+    # --------------- MACHINE INITIALIZATION FUNCTION ---------------            
+    def run(self):
+        while not rospy.is_shutdown():
+            self.zero()
+
+    # --------------- CHECK ROSPY FUNCTION DEFINITION --------------- 
+    def check_rospy(self):
+        
+        while not rospy.is_shutdown():
+            rospy.sleep(0.1)
+            
+        print(self.consoleFormatter.format("Shutting down", "FAIL"))
+        os._exit(os.EX_OK)
+
+# --------------- INITIALIZATION OF THE TASK CLASS CONSTRUCTOR --------------- 
+if __name__ == "__main__":
+    sm = CARRY_MY_LUGGAGE()
+    sm.run()
+    rospy.spin()
